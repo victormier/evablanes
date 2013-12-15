7399a1d09bf5aa26c54f4ea00f016fe2f20f23b9
Victor Mier <victormier@gmail.com>
2013-12-15 12:56:04 -0800

    Add some markup and styles to homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,2 +1,6 @@
 class StaticController < ApplicationController
+  def index
+    @slider_projects = Project.published.in_slider.ordered
+    @featured_projects = Project.published.featured.ordered.limit(3)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def view_id
     "#{controller.controller_name} #{controller.action_name.downcase}"
   end
+
+  def active_link_to(name, options = {}, html_options = {})
+    if html_options.key?(:class)
+      html_options.merge!({ :class => html_options[:class] + ' active' }) if current_page?(options)
+    else
+      html_options.merge!({ :class => 'active' }) if current_page?(options)
+    end
+    link_to name, options, html_options
+  end
 end
