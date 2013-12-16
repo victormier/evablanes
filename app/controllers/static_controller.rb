c3b5a48f532392e9f4db99b90efce544f68cd8f2
Victor Mier <victormier@gmail.com>
2013-12-15 17:09:05 -0800

    Add some more modules to home

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,5 +2,6 @@ class StaticController < ApplicationController
   def index
     @slider_projects = Project.published.in_slider.ordered
     @featured_projects = Project.published.featured.ordered.limit(3)
+    @some_projects = Project.published.ordered.limit(8)
   end
 end
