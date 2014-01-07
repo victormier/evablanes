a09887f55516824f119dfa358e45bbbc8def7f35
Victor Mier <victormier@gmail.com>
2014-01-06 20:07:28 -0800

    Add locale links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,36 @@ module ApplicationHelper
     end
     link_to name, options, html_options
   end
+
+  def locale_url(opts)
+    current_locale = I18n.locale
+
+    I18n.with_locale(opts[:i18n_locale].presence || I18n.locale) do
+      if has_id && is_show && model = has_model(Project)
+        instance = I18n.with_locale(current_locale) do
+          model.find_for_show(params[:id])
+        end
+
+        opts[:id] = instance.to_param
+      end
+
+      url_for(opts)
+    end
+  end
+
+  def has_id
+    params[:id].present?
+  end
+
+  def is_show
+    params[:action] == 'show'
+  end
+
+  def has_model(*models)
+    models.detect { |model| model.to_s.tableize == params[:controller] }
+  end
+
+  def has_param(param)
+    params[param].present?
+  end
 end
