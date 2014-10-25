ed9c664fde04112dd84d70bc01840788fff0d12f
Victor Mier <victormier@gmail.com>
2014-10-24 17:36:25 -0700

    Mail preview only if development!

--- app/mailers/mail_preview.rb
+++ app/mailers/mail_preview.rb
@@ -1,6 +1,8 @@
-class MailPreview < MailView
-  # Pull data from existing fixtures
-  def newsletter
-    Notifier.newsletter
+if Rails.env.development?
+  class MailPreview < MailView
+    # Pull data from existing fixtures
+    def newsletter
+      Notifier.newsletter
+    end
   end
 end
