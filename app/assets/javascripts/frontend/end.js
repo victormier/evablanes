f6e93191278451c1a7d21613d7e3fd18ae76faee
victormier <victormier@gmail.com>
2017-04-28 20:36:53 +0200

    Add back to top arrow

--- app/assets/javascripts/frontend/end.js
+++ app/assets/javascripts/frontend/end.js
@@ -9,4 +9,24 @@ $(function() {
   })
 
   $('textarea').autoGrow();
+
+  if($('.photo-list').length) {
+    var leftPos = $('.photo-list').position().left + $('.photo-list').width() + 50,
+        $backToTop = $('#back-to-top');
+
+    $backToTop.css("left", leftPos).on('click', function(e) {
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop : 0
+      }, 500);
+    });
+  }
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {
+      $('#back-to-top').fadeIn(200);
+    } else {
+      $('#back-to-top').fadeOut(200);
+    }
+  });
 });
